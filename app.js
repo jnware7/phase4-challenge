984b258ecde3e13edec42557a564cd196f322782
jnware7 <jnware7@gmail.com>
2017-08-14 10:36:37 -0700

    setup express server and view engine

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const http = require('http');
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+
+const port = process.env.PORT || '3000';
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}...`)
+})
