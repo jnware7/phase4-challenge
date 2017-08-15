2ac9828578fe8d281717917dbc588c2f73c6c417
jnware7 <jnware7@gmail.com>
2017-08-14 17:13:24 -0700

    added database scripts to package json

--- db/database.js
+++ db/database.js
@@ -1,5 +1,5 @@
 const pgp = require( 'pg-promise' )()
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/phase4-challenge'
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/vinyl2'
 const db = pgp( connectionString )
 
 module.exports = db
