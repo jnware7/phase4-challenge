6e63a76c98566e2838e5cb8647146fa1fa3ca87e
jnware7 <jnware7@gmail.com>
2017-08-15 21:33:05 -0700

    fixed index and albums page

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,30 @@ const {createReview,
 }  = require('../db/reviews')
 
 
-router.get('/', function(req, res) {
-  getAllAlbums()
-  .then( albums =>{
-    res.render('index', {albums: albums});
-  }).catch(err => {
-    throw err
-    console.error((err.message), {albums: albums} )
-  })
-});
+// router.get('/', function(req, res) {
+//   getAllAlbums()
+//   .then( albums =>{
+//     res.render('index', {albums: albums});
+//   }).catch(err => {
+//     throw err
+//     console.error((err.message), {albums: albums} )
+//   })
+// });
 
+router.get('/', (req, res) => {
+  Promise.all([
+    getAllAlbums(),
+    getRecentReview()
+  ])
+  .then(results => {
+    const albums = results[0]
+    const reviews = results[1]
+    res.render('index', {
+      albums: albums,
+      reviews: reviews
+    })
+  }).catch(err => {
+    console.error(err)
+  })
+})
 module.exports = router;
